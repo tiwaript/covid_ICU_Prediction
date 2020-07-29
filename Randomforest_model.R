ad70fcaa07510273b1ec819032d4e8529ba22504
tiwaript <pradeep.tiwari@igib.in>
2020-07-29 11:09:30 +0530

    edited on 280720

--- Randomforest_model.R
+++ Randomforest_model.R
@@ -32,3 +32,62 @@ rf_model<-randomForest(x=data_training[,-c(1:3)],y=data_training[,3],ntree=10000
 pred_class<-predict(rf_model,data_testing[,-c(1:3)])
 obs_class<-data_testing[,3]
 conf_mat<-confusionMatrix(pred_class,obs_class)
+## equal sampling #####
+index_noicu<-which(data_covid_wc$icugr_r=="No")
+sample_noicu_60<-sample(index_noicu,size=60,replace = F)
+index_icu<-which(data_covid_wc$icugr_r=="Yes")
+data<-data_covid_wc[c(sample_noicu_60,index_icu),]
+
+index_noicu<-which(data$icugr_r=="No")
+sample_noicu<-sample(index_noicu,size = 0.7*length(index_noicu),replace=F)
+
+index_icu<-which(data$icugr_r=="Yes")
+sample_icu<-sample(index_icu,size = 0.7*length(index_icu),replace=F)
+
+data_training<-data[c(sample_icu,sample_noicu),]
+data_testing<-data[-c(sample_icu,sample_noicu),]
+
+rf_model<-randomForest(x=data_training[,-c(1:3)],y=data_training[,3],ntree=10000,mtry=4,data=data_training)
+pred_class<-predict(rf_model,data_testing[,-c(1:3)])
+obs_class<-data_testing[,3]
+conf_mat<-confusionMatrix(pred_class,obs_class)
+conf_mat
+############# excluding crp and ferritin
+index_noicu<-which(data_covid_wc$icugr_r=="No")
+sample_noicu_60<-sample(index_noicu,size=60,replace = F)
+index_icu<-which(data_covid_wc$icugr_r=="Yes")
+data<-data_covid_wc[c(sample_noicu_60,index_icu),]
+
+index_noicu<-which(data$icugr_r=="No")
+sample_noicu<-sample(index_noicu,size = 0.7*length(index_noicu),replace=F)
+
+index_icu<-which(data$icugr_r=="Yes")
+sample_icu<-sample(index_icu,size = 0.7*length(index_icu),replace=F)
+
+data_training<-data[c(sample_icu,sample_noicu),]
+data_testing<-data[-c(sample_icu,sample_noicu),]
+
+rf_model<-randomForest(x=data_training[,-c(1:3,9:10)],y=data_training[,3],ntree=10000,mtry=4,data=data_training)
+pred_class<-predict(rf_model,data_testing[,-c(1:3,9:10)])
+obs_class<-data_testing[,3]
+conf_mat<-confusionMatrix(pred_class,obs_class)
+conf_mat
+index_noicu<-which(data_covid_wc$icugr_r=="No")
+sample_noicu_60<-sample(index_noicu,size=60,replace = F)
+index_icu<-which(data_covid_wc$icugr_r=="Yes")
+data<-data_covid_wc[c(sample_noicu_60,index_icu),]
+
+index_noicu<-which(data$icugr_r=="No")
+sample_noicu<-sample(index_noicu,size = 0.7*length(index_noicu),replace=F)
+
+index_icu<-which(data$icugr_r=="Yes")
+sample_icu<-sample(index_icu,size = 0.7*length(index_icu),replace=F)
+
+data_training<-data[c(sample_icu,sample_noicu),]
+data_testing<-data[-c(sample_icu,sample_noicu),]
+
+rf_model<-randomForest(x=data_training[,-c(1:3)],y=data_training[,3],ntree=10000,mtry=4,data=data_training)
+pred_class<-predict(rf_model,data_testing[,-c(1:3)])
+obs_class<-data_testing[,3]
+conf_mat<-confusionMatrix(pred_class,obs_class)
+conf_mat
